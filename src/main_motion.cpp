978fad63486bc261ac2e4d0363ca85d13545ea98
Lorenzo D'Errico <lorenzo.derrico@unina.it>
2025-06-19 12:51:39 +0200

    Update main_motion.cpp

--- src/main_motion.cpp
+++ src/main_motion.cpp
@@ -20,6 +20,7 @@
 #include <memory>
 #include <chrono>
 #include <vector>
+#include <iostream>
 
 int main(int argc, char ** argv)
 {
@@ -31,8 +32,8 @@ int main(int argc, char ** argv)
   node->GripperControl("OPEN");
   // You can spin in a separate thread or just call your function directly
   try {
-    double lift_value = 0.0;   // Example lift value within limits
-    // node->TorsoControl(lift_value);
+    double lift_value = 5.0;   // Example lift value within limits
+    node->TorsoControl(lift_value);
   } catch (const std::exception & e) {
     RCLCPP_ERROR(node->get_logger(), "Exception in TorsoControl: %s ", e.what());
   }
@@ -52,7 +53,7 @@ int main(int argc, char ** argv)
   obstacle_pose.pose.orientation.w = 1.0;
   moveit_msgs::msg::PlanningScene planning_scene_msg =
     node->Add_Obstacle(obstacle_pose, "Table");
-  //planning_scene_publisher_->publish(planning_scene_msg);
+  planning_scene_publisher_->publish(planning_scene_msg);
 
 
   geometry_msgs::msg::Pose target_pose;
@@ -62,9 +63,21 @@ int main(int argc, char ** argv)
 
   // Simple orientation (quaternion), facing forward
   target_pose.orientation.x = 0.0;
-  target_pose.orientation.y = 0.0;
+  target_pose.orientation.y = 0.7071068;  
   target_pose.orientation.z = 0.0;
-  target_pose.orientation.w = 1.0;
+  target_pose.orientation.w = 0.7071068;
+  
+  geometry_msgs::msg::Pose target_pose2;
+  target_pose2.position.x = 0.4;
+  target_pose2.position.y = 0.0;
+  target_pose2.position.z = 0.5;
+
+  // Simple orientation (quaternion), facing forward
+  target_pose2.orientation.x = 0.5;
+  target_pose2.orientation.y = 0.5;  
+  target_pose2.orientation.z = 0.5;
+  target_pose2.orientation.w = 0.5;
+
 
   try {
     node->motion_planning_control(target_pose, RobotTaskStatus::Arm::ARM_torso);  // or "arm" depending on your group name
@@ -72,9 +85,47 @@ int main(int argc, char ** argv)
   } catch (const std::exception & e) {
     RCLCPP_ERROR(node->get_logger(), "Motion planning failed: %s", e.what());
   }
+  char input = 'a';
 
-
+  while (input != ' ')
+  {
+      cout << "Press Space to close the gripper: ";
+      cin >> input;
+  }
+  // vector<int> v = {0, 1, 2};
+  // int state = 0;
   node->GripperControl("CLOSE");
+
+  try {
+    node->motion_planning_control(target_pose2, RobotTaskStatus::Arm::ARM_torso);  // or "arm" depending on your group name
+    RCLCPP_INFO(node->get_logger(), "Motion planning succeeded.");
+  } catch (const std::exception & e) {
+    RCLCPP_ERROR(node->get_logger(), "Motion planning failed: %s", e.what());
+  }
+  char input = 'a';
+
+  while (input != ' ')
+  {
+      cout << "Press Space to close the gripper: ";
+      cin >> input;
+  }
+  // vector<int> v = {0, 1, 2};
+  // int state = 0;
+  node->GripperControl("OPEN"); 
+  // while (true)
+  // {
+
+  //   if (input == ' ') {
+  //       cout << "Hai premuto spazio." << endl;
+  //   } else if (input == 'y') {
+  //       cout << "Hai premuto y." << endl;
+  //   } else if (input == 'n') {
+  //       cout << "Hai premuto n." << endl;
+  //   } else {
+  //       cout << "Hai premuto: " << input << endl;
+  //   }
+  // }
+
 
   rclcpp::shutdown();
   return 0;
