013f2994eb924877cda2389b5649498071a40484
Lorenzo D'Errico <lorenzo.derrico@unina.it>
2025-06-20 10:54:37 +0200

    State Machine first commit

--- src/main_motion.cpp
+++ src/main_motion.cpp
@@ -32,8 +32,8 @@ int main(int argc, char ** argv)
   node->GripperControl("OPEN");
   // You can spin in a separate thread or just call your function directly
   try {
-    double lift_value = 5.0;   // Example lift value within limits
-    node->TorsoControl(lift_value);
+    double lift_value = 0.0;   // Example lift value within limits
+    // node->TorsoControl(lift_value);
   } catch (const std::exception & e) {
     RCLCPP_ERROR(node->get_logger(), "Exception in TorsoControl: %s ", e.what());
   }
@@ -42,41 +42,70 @@ int main(int argc, char ** argv)
     "/planning_scene", rclcpp::QoS(1));
 
 // Add obj table in pose 0.4 0.0 0.5
-  geometry_msgs::msg::PoseStamped obstacle_pose;
-  obstacle_pose.header.frame_id = "base_footprint";
-  obstacle_pose.pose.position.x = 0.4;
-  obstacle_pose.pose.position.y = 0.0;
-  obstacle_pose.pose.position.z = 0.5;
-  obstacle_pose.pose.orientation.x = 0.0;
-  obstacle_pose.pose.orientation.y = 0.0;
-  obstacle_pose.pose.orientation.z = 0.0;
-  obstacle_pose.pose.orientation.w = 1.0;
-  moveit_msgs::msg::PlanningScene planning_scene_msg =
-    node->Add_Obstacle(obstacle_pose, "Table");
-  planning_scene_publisher_->publish(planning_scene_msg);
+  // geometry_msgs::msg::PoseStamped obstacle_pose;
+  // obstacle_pose.header.frame_id = "base_footprint";
+  // obstacle_pose.pose.position.x = 0.4;
+  // obstacle_pose.pose.position.y = 0.0;
+  // obstacle_pose.pose.position.z = 0.5;
+  // obstacle_pose.pose.orientation.x = 0.0;
+  // obstacle_pose.pose.orientation.y = 0.0;
+  // obstacle_pose.pose.orientation.z = 0.0;
+  // obstacle_pose.pose.orientation.w = 1.0;
+  // moveit_msgs::msg::PlanningScene planning_scene_msg =
+  //   node->Add_Obstacle(obstacle_pose, "Table");
+  // planning_scene_publisher_->publish(planning_scene_msg);
+  vector<float> marker = {0.1,0.2,0.4};
+  float gripper= 0.235;
+  float layer = 0.015
+  vector<float> goal_y = {0.025, -0.025, 0, 0} + marker[1];
+  vector<float> goal_x = {0.0, 0.0, 0.025, -0.025}+ marker[0];
+  vector<float> goal_z = {0.0, 0.0, layer, layer}+marker[2]+gripper;
+  vector<float> orizontal = {-0.5, -0.5, 0.5, -0.5};
+  vector<float> vertical = {0.0, 0.7071068, 0.0, 0.7071068};
 
 
   geometry_msgs::msg::Pose target_pose;
-  target_pose.position.x = 0.4;
-  target_pose.position.y = 0.0;
-  target_pose.position.z = 0.5;
+  target_pose.position.x = goal_x[0]; 
+  target_pose.position.y = goal_y[0];  
+  target_pose.position.z = goal_z[0];  
 
   // Simple orientation (quaternion), fastd::cing forward
-  target_pose.orientation.x = 0.0;
-  target_pose.orientation.y = 0.7071068;  
-  target_pose.orientation.z = 0.0;
-  target_pose.orientation.w = 0.7071068;
+  target_pose.orientation.x = vertical[0];
+  target_pose.orientation.y = vertical[1] ;  
+  target_pose.orientation.z = vertical[2];
+  target_pose.orientation.w = verttical[3];
+  // geometry_msgs::msg::Pose target_pose2;
+  // target_pose2.position.x = 0.4;
+  // target_pose2.position.y = 0.0;
+  // target_pose2.position.z = 0.5;
+
+  // // Simple orientation (quaternion), fastd::cing forward
+  // target_pose2.orientation.x = 0.0;
+  // target_pose2.orientation.y = 0.7071068;  
+  // target_pose2.orientation.z = 0.0;
+  // target_pose2.orientation.w = 0.7071068;
   
-  geometry_msgs::msg::Pose target_pose2;
-  target_pose2.position.x = 0.4;
-  target_pose2.position.y = 0.0;
-  target_pose2.position.z = 0.5;
+  // geometry_msgs::msg::Pose target_pose3;
+  // target_pose3.position.x = 0.4;
+  // target_pose3.position.y = 0.0;
+  // target_pose3.position.z = 0.5;
 
-  // Simple orientation (quaternion), fastd::cing forward
-  target_pose2.orientation.x = 0.5;
-  target_pose2.orientation.y = 0.5;  
-  target_pose2.orientation.z = 0.5;
-  target_pose2.orientation.w = 0.5;
+  // // Simple orientation (quaternion), fastd::cing forward
+  // target_pose3.orientation.x = 0.5;
+  // target_pose3.orientation.y = 0.5;  
+  // target_pose3.orientation.z = 0.5;
+  // target_pose3.orientation.w = 0.5;
+
+  // geometry_msgs::msg::Pose target_pose4;
+  // target_pose4.position.x = 0.4;
+  // target_pose4.position.y = 0.0;
+  // target_pose4.position.z = 0.5;
+
+  // // Simple orientation (quaternion), fastd::cing forward
+  // target_pose4.orientation.x = 0.5;
+  // target_pose4.orientation.y = 0.5;  
+  // target_pose4.orientation.z = 0.5;
+  // target_pose4.orientation.w = 0.5;
 
 
   try {
@@ -87,13 +116,13 @@ int main(int argc, char ** argv)
   }
   char input = 'a';
 
-  while (input != ' ')
+  while (input != 'g')
   {
       std::cout << "Press Space to close the gripper: ";
       std::cin >> input;
   }
-  // vector<int> v = {0, 1, 2};
-  // int state = 0;
+  vector<int> v = {0, 1, 2};
+  int state = 0;
   node->GripperControl("CLOSE");
 
   try {
@@ -108,9 +137,9 @@ int main(int argc, char ** argv)
       std::cout << "Press Space to close the gripper: ";
       std::cin >> input;
   }
+  node->GripperControl("OPEN"); 
   // vector<int> v = {0, 1, 2};
   // int state = 0;
-  node->GripperControl("OPEN"); 
   // while (true)
   // {
 
